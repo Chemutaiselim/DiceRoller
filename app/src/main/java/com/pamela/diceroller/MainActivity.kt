c5effe305b1078a02c367bc7d612c4a6d613c3a9
Selim <selimchemutai19@gmail.com>
2022-01-08 10:57:59 +0300

    Update MainActivity.kt

--- app/src/main/java/com/pamela/diceroller/MainActivity.kt
+++ app/src/main/java/com/pamela/diceroller/MainActivity.kt
@@ -33,7 +33,18 @@ class MainActivity : AppCompatActivity() {
         val diceImage:ImageView = findViewById(R.id.imageView)
 
         //The setImageResource() method is passing the resource id for the dice_2 image.
-        diceImage.setImageResource(R.drawable.dice_2)
+        //diceImage.setImageResource(R.drawable.dice_2)
+
+        //instead of using if statement...
+        //it's read as when diceRoll is 1 display the drawable..
+        when (diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2-> diceImage.setImageResource(R.drawable.dice_2)
+            3-> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 
